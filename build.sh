aea20710cb57fa53d8de93107aee31f8909a7273
Libo Zeng <ringil.sword@gmail.com>
2022-09-10 12:10:06 -0400

    add this

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 buf generate
 CGO_ENABLED=0 GOOS=linux go build -o ./benchmark-japan-vps -a -ldflags '-w -s' ./server/main.go
 tar -czf benchmark-japan-vps.tar.gz benchmark-japan-vps bench.sh parse_benchmark_result.sh run_and_parse_bench.sh
